58db30960941ec1d37ce361e86510a068104856f
Max Franz <maxkfranz@gmail.com>
2019-03-19 14:05:46 -0400

    Use more general xml parsing
    TODOs
    - Make sure the insertion into index is enabled & working
    - General testing
    - Blacklist expansion, only if performance is an issue
    - Update parser.js so most code from uniprot goes there

--- src/server/datasource/uniprot.js
+++ src/server/datasource/uniprot.js
@@ -6,6 +6,7 @@ const db = require('../db');
 const path = require('path');
 const download = require('./download');
 const logger = require('../logger');
+const Future = require('fibers/future');
 
 const FILE_PATH = path.join(INPUT_PATH, UNIPROT_FILE_NAME);
 const ENTRY_NS = 'uniprot';
@@ -28,14 +29,69 @@ const XML_TAGS = Object.freeze({
   SHORT_NAME: 'shortName'
 });
 
+const UNSTORED_XML_TAGS = [XML_TAGS.UNIPROT];
+
+const pushIfNonNil = ( arr, val ) => {
+  if( val ){
+    arr.push( val );
+  }
+};
+
+const getShortOrFullName = n => getShortName( n ) || getFullName( n );
+const getShortName = n => _.get(n, ['shortName', 0, '$text']) || _.get(n, ['shortName', 0]);
+const getFullName = n => _.get(n, ['fullName', 0, '$text']) || _.get(n, ['fullName', 0]);
+
+const processEntry = entry => {
+  let namespace = ENTRY_NS;
+  let type = ENTRY_TYPE;
+  let id = _.get( entry, [ 'accession', 0 ] );
+  let name = _.get( entry, 'name' );
+  let organism = _.get( entry, [ 'organism', 'dbReference', '$', 'id' ] );
+  let geneNames = _.get( entry, [ 'gene', 'name' ], [] ).map( res => res['$text'] );
+  let recProtName = _.get( entry, [ 'protein', 'recommendedName' ] );
+  let recFullProteinName = getFullName( recProtName );
+  let recShortProteinName = getShortName( recProtName );
+  let altProteinNames = _.get( entry, [ 'protein', 'alternativeName' ], [] ).map( getShortOrFullName );
+  let subProteinNames = _.get( entry, [ 'protein', 'submittedName' ], [] ).map( getShortOrFullName );
+
+  let proteinNames = [];
+  pushIfNonNil( proteinNames, recShortProteinName );
+  pushIfNonNil( proteinNames, recFullProteinName );
+  altProteinNames.forEach( name => pushIfNonNil( proteinNames, name ) );
+  subProteinNames.forEach( name => pushIfNonNil( proteinNames, name ) );
+
+  // since uniprot uses weird names, use the first "protein name" instead, if possible
+  name = proteinNames[0] || name;
+
+  return { namespace, type, id, organism, name, geneNames, proteinNames };
+};
+
+const processChunk = chunk => {
+  let task = Future.wrap(function(chunk, next){ // code in this block runs in its own thread
+    let processedEntries = chunk.map(processEntry);
+    let err = null;
+
+    next( err, processedEntries );
+  });
+
+  return task(chunk).promise();
+};
+
 const updateFromFile = function(){
   return new Promise( resolve => {
     let entries = [];
     let process = Promise.resolve();
 
-    const insertChunk = chunk => db.insertEntries( chunk, false );
+    // TODO
+    // const insertChunk = chunk => db.insertEntries( chunk, false );
+
+    const insertChunk = chunk => {
+      console.log('insertChunk', chunk);
+    };
 
     const enqueueEntry = entry => {
+      console.log('enqueue', entry);
+
       entries.push(entry);
 
       if( entries.length >= ENTRIES_CHUNK_SIZE ){
@@ -49,146 +105,78 @@ const updateFromFile = function(){
       let chunk = entries;
       entries = [];
 
-      process = process.then( () => insertChunk( chunk ) );
+      process = process.then(() => processChunk(chunk)).then(insertChunk);
 
       return process;
     };
 
     const consumeEntry = entry => {
-      const orgId = entry.organism;
+      const orgId = null;
 
+      // require('fs').writeFileSync('entry.json', JSON.stringify(entry, null, 2));
+
+      // TODO
       // consider only the supported organisms
-      if ( isSupportedOrganism( orgId ) ){
+      // if ( isSupportedOrganism( orgId ) ){
         enqueueEntry(entry);
-      }
+      // }
+    };
+
+    const shouldStoreNode = node => {
+      return !UNSTORED_XML_TAGS.some(tag => tag === node.name);
     };
 
     const parseXml = () => {
       let tagStack = [];
-      let entry;
-
-      // alternative and submitted name objects for current protein,
-      // possible properties of these objects are SHORT_NAME and FULL_NAME tag names,
-      // these variables are maintained because only one of SHORT_NAME or FULL_NAME
-      // will be pushed to proteinNames when the related tag is closed
-      let alternativeProtName = {};
-      let submittedProtName = {};
-
-      const makeEmptyEntry = () => {
-        let proteinNames = [];
-        let geneNames = [];
-        let namespace = ENTRY_NS;
-        let type = ENTRY_TYPE;
-
-        return { proteinNames, geneNames, namespace, type };
-      };
-
-      const pushToProtNames = n => {
-        entry.proteinNames.push( n );
-      };
-
-      const pushToGeneNames = n => {
-        entry.geneNames.push( n );
-      };
-
-      const setId = id => {
-        if ( _.isNil( entry.id ) ) {
-          entry.id = id;
-        }
-      };
-
-      const setName = n => {
-        entry.name = n;
-      };
-
-      const setOrganism = o => {
-        entry.organism = o;
-      };
-
-      const getTagAt = index => {
-        if ( index < 0 ) {
-          index += tagStack.length;
-        }
-
-        return _.get( tagStack, index );
-      };
+      let top = () => tagStack[tagStack.length - 1];
 
       const onopentag = node => {
-        let last = node.name;
-        let attributes = node.attributes;
+        let parent = top();
+        let hasParent = parent != null;
+        let { attributes, name } = node;
 
-        tagStack.push( last );
-        let prev1 = getTagAt( -2 );
-
-        if ( last == XML_TAGS.ENTRY && prev1 == XML_TAGS.UNIPROT ) {
-          entry = makeEmptyEntry();
-        }
-        else if ( last == XML_TAGS.DB_REFERENCE && prev1 == XML_TAGS.ORGANISM ) {
-          setOrganism( attributes.id );
-        }
-      };
-
-      const onclosetag = node => {
-        let tag = node.name;
-        let selectAndPush = obj => {
-          let name = obj[XML_TAGS.SHORT_NAME] || obj[XML_TAGS.FULL_NAME];
-          if ( !_.isNil( name ) ){
-            pushToProtNames( name );
-          }
+        let parsedNode = {
+          name,
+          attributes,
+          children: []
         };
 
-        if ( tag == XML_TAGS.ALTERNATIVE_NAME ) {
-          selectAndPush( alternativeProtName );
-          alternativeProtName = {};
-        }
-        else if ( tag == XML_TAGS.SUBMITTED_NAME ) {
-          selectAndPush( submittedProtName );
-          submittedProtName = {};
-        }
-        else if ( tag == XML_TAGS.ENTRY ) {
-          consumeEntry( entry );
-        }
+        if( shouldStoreNode(parsedNode) ){
+          if( hasParent ){
+            parent.children.push(parsedNode);
+          }
 
-        tagStack.pop();
+          tagStack.push(parsedNode);
+        }
       };
 
       const ontext = text => {
+        let topTag = top();
+
+        if( topTag == null ){ // omit if unstored
+          return;
+        }
+
         // omit if text is full of white spaces
         if ( /^\s*$/.test(text) ) {
           return;
         }
 
-        let last = getTagAt( -1 );
-        let prev1 = getTagAt( -2 );
-        let prev2 = getTagAt( -3 );
+        topTag.text = text;
+      };
 
-        let isFullOrShortNameTag =
-          tagName => tagName == XML_TAGS.FULL_NAME || tagName == XML_TAGS.SHORT_NAME;
+      const onclosetag = node => {
+        let topTag = top();
 
-        if (  prev1 == XML_TAGS.ENTRY ) {
-          if ( last == XML_TAGS.ACCESSION ) {
-            setId( text );
-          }
-          else if ( last == XML_TAGS.NAME ) {
-            setName( text );
-          }
+        if( topTag == null ){ // omit if unstored
+          return;
         }
-        else if ( prev1 == XML_TAGS.GENE && last == XML_TAGS.NAME ) {
-          pushToGeneNames( text );
+
+        if( topTag.name === XML_TAGS.ENTRY ){
+          consumeEntry(topTag);
         }
-        else if ( prev2 == XML_TAGS.PROTEIN && isFullOrShortNameTag( last ) ) {
-          if ( prev1 == XML_TAGS.RECOMMENDED_NAME ) {
-            // for RECOMMENDED_NAME both of FULL_NAME and SHORT_NAME must be pushed
-            // so no need for checking last tag name here
-            pushToProtNames( text );
-          }
-          else if ( prev1 == XML_TAGS.SUBMITTED_NAME ) {
-            submittedProtName[ last ] = text;
-          }
-          else if ( prev1 == XML_TAGS.ALTERNATIVE_NAME ) {
-            alternativeProtName[ last ] = text;
-          }
-        }
+
+        tagStack.pop();
       };
 
       const onend = () => {
@@ -212,7 +200,7 @@ const updateFromFile = function(){
     const guaranteeIndex = () => db.guaranteeIndex();
     const disableAutoRefresh = () => db.disableAutoRefresh();
     const clearNamespace = () => db.clearNamespace(ENTRY_NS);
-    
+
     guaranteeIndex()
       .then( clearNamespace )
       .then( disableAutoRefresh )
