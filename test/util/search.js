c6eeec469657eb931b1e62713bf3fa774a4ef592
Max Franz <maxkfranz@gmail.com>
2019-04-18 12:08:12 -0400

    Add top 10 gene symbols from Nature -- 100% effectiveness at getting the correct result in the top entry

--- test/util/search.js
+++ test/util/search.js
@@ -1,5 +1,29 @@
 // name (search string) : grounding object { namespace, id }
 const SEARCH_OBJECT = Object.freeze({
+
+  // here are some common gene symbols to get started
+  // https://www.nature.com/articles/d41586-017-07291-9
+  'p53': { namespace: 'ncbi', id: '7157' },
+  'tp53': { namespace: 'ncbi', id: '7157' },
+  'brca2': { namespace: 'ncbi', id: '675' },
+  'brca1': { namespace: 'ncbi', id: '672' },
+  'tnf': { namespace: 'ncbi', id: '7124' },
+  'egfr': { namespace: 'ncbi', id: '1956' },
+  'vegfa': { namespace: 'ncbi', id: '7422' },
+  'apoe': { namespace: 'ncbi', id: '348' },
+  'il6': { namespace: 'ncbi', id: '3569' },
+  'tgfb1': { namespace: 'ncbi', id: '7040' },
+  'esr1': { namespace: 'ncbi', id: '2099' },
+  'akt1': { namespace: 'ncbi', id: '207' },
+  'hla-drb1': { namespace: 'ncbi', id: '3123' },
+  'nfkb1': { namespace: 'ncbi', id: '4790' },
+  'ace': { namespace: 'ncbi', id: '1636' },
+
+
+  // TODO change the uniprot groundings to ncbi
+
+  // Jeff's case studies:
+
   // Yang et al. Molecular Cell, Volume 53, Issue 1, 9 January 2014, Pages 88-100
   'hypoxia': null,
   'HIF-1 alpha': { namespace: 'uniprot', id: 'Q16665' }, // example grounding
