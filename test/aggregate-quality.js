8dd8297e0c6939123b23d6d4571fec81bfd26570
jvwong <jvwong@gmail.com>
2019-04-24 13:02:00 -0400

    Updating search test test data access.

--- test/aggregate-quality.js
+++ test/aggregate-quality.js
@@ -1,7 +1,7 @@
 import { expect } from './util/chai';
 import { aggregate, applyToEachDS } from './util/datasource';
 import { forceDownload, buildIndex } from './util/param';
-import { SEARCH_OBJECT } from './util/search';
+import { SEARCH_OBJECTS } from './util/search';
 import { db } from '../src/server/db';
 
 const updateTestData = () => {
@@ -14,8 +14,6 @@ const searchEnt = name => aggregate.search( name );
 const getEnt = (ns, id) => aggregate.get( ns, id );
 const removeTestIndex = () => db.deleteIndex();
 
-const GENE_LIST = Object.keys( SEARCH_OBJECT );
-
 describe('Search and Get Aggregate', function(){
   this.timeout(10000);
 
@@ -24,37 +22,43 @@ describe('Search and Get Aggregate', function(){
     after(removeTestIndex);
   }
 
-  GENE_LIST.forEach( name => {
-    const expectedGrounding = SEARCH_OBJECT[name]; // could be null b/c we haven't specified groundings yet...
+  
+  
+  SEARCH_OBJECTS.forEach( testCase => {
+    const { id, entities } = testCase;
 
-    if( !expectedGrounding ){ return; } // skip if no grounding specified
-
-    const { id, namespace } = expectedGrounding;
-
-    it(`search ${name}`, function(){
-      return ( searchEnt(name)
-        .then( results => {
-          expect(results).to.exist;
-
-          let firstResult = results[0];
-
-          expect(firstResult).to.exist;
-          expect(firstResult.namespace, 'namespace').to.equal(namespace);
-          expect(firstResult.id, 'id').to.equal(id);
-        } )
-      );
+    describe(`Testing id ${id}`, function(){
+      entities.forEach( entity => {
+        const { text, xref_id, namespace } = entity;
+  
+        if( !xref_id ){ return; } // skip if no grounding specified
+        
+        it(`search ${text}`, function(){
+          return ( searchEnt(text)
+            .then( results => {
+              expect( results ).to.exist;
+    
+              let firstResult = results[0];
+    
+              expect( firstResult ).to.exist;
+              expect( firstResult.namespace, 'namespace' ).to.equal( namespace );
+              expect( firstResult.id, 'id' ).to.equal( xref_id );
+            } )
+          );
+        });
+    
+        it(`get ${text}`, function(){
+          return ( getEnt( namespace, xref_id )
+            .then( result => {
+              expect( result, 'result' ).to.exist;
+              expect( result.namespace, 'namespace' ).to.equal( namespace );
+              expect( result.id, 'id' ).to.equal( xref_id );
+            } )
+          );
+        });
+      }); // entities
     });
 
-    it(`get ${name}`, function(){
-      return ( getEnt(namespace, id)
-        .then( result => {
-          expect(result, 'result').to.exist;
-          expect(result.namespace, 'namespace').to.equal(namespace);
-          expect(result.id, 'id').to.equal(id);
-        } )
-      );
-    });
-
-  }); // for each
+  }); // SEARCH_OBJECTS
 
 }); // describe
--- test/util/search.js
+++ test/util/search.js
@@ -1,26 +1,11 @@
-// name (search string) : grounding object { namespace, id }
-const SEARCH_OBJECT = Object.freeze({
+const _ = require('lodash');
 
-  // here are some common gene symbols to get started
-  // https://www.nature.com/articles/d41586-017-07291-9
-  'p53': { namespace: 'ncbi', id: '7157' },
-  'tp53': { namespace: 'ncbi', id: '7157' },
-  'brca2': { namespace: 'ncbi', id: '675' },
-  'brca1': { namespace: 'ncbi', id: '672' },
-  'tnf': { namespace: 'ncbi', id: '7124' },
-  'egfr': { namespace: 'ncbi', id: '1956' },
-  'vegfa': { namespace: 'ncbi', id: '7422' },
-  'apoe': { namespace: 'ncbi', id: '348' },
-  'il6': { namespace: 'ncbi', id: '3569' },
-  'tgfb1': { namespace: 'ncbi', id: '7040' },
-  'esr1': { namespace: 'ncbi', id: '2099' },
-  'akt1': { namespace: 'ncbi', id: '207' },
-  'hla-drb1': { namespace: 'ncbi', id: '3123' },
-  'nfkb1': { namespace: 'ncbi', id: '4790' },
-  'ace': { namespace: 'ncbi', id: '1636' },
+const MOLECULAR_CELL = require('./data/molecular-cell.json');
+const TOP_NCBI_GENE = require('./data/top-ncbi-gene.json');
 
-  // an example chemical
-  'aspirin': { namespace: 'chebi', id: 'CHEBI:15365' }
-});
+const SEARCH_OBJECTS = _.concat( [], 
+    // MOLECULAR_CELL, 
+    TOP_NCBI_GENE 
+  );
 
-export { SEARCH_OBJECT };
+export { SEARCH_OBJECTS };
