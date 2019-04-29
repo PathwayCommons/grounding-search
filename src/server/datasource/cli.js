2f6dcd42f53660c8eb0338a232fadeb0c791999a
Max Franz <maxkfranz@gmail.com>
2019-04-29 18:16:15 -0400

    clear script optimisation

--- src/server/datasource/cli.js
+++ src/server/datasource/cli.js
@@ -5,6 +5,7 @@ import logger from '../logger';
 import { uniprot } from './uniprot';
 import { chebi } from './chebi';
 import { ncbi } from './ncbi';
+import { db } from '../db';
 
 const sources = { uniprot, chebi, ncbi };
 const op = process.argv[2];
@@ -16,13 +17,16 @@ if( !validOps.some(vo => vo === op) ){
   logger.error(`Op '${op}' not supported; try 'update' or 'clear'`);
 }
 
-if( source == null ){
+if( op === 'clear' && passedSourceId === 'all' ){
+  logger.info('Clearing entire index');
+  db.deleteIndex().then(() => logger.info('Successfully cleared entire index'));
+} else if( source == null ){
   logger.error(`No source '${passedSourceId}' found`);
 } else {
   logger.info(`Applying ${op} on source '${passedSourceId}'...`);
 
   let fcn = source[op];
-  
+
   fcn().then(() => {
     logger.info(`Successfully applied ${op} on source '${passedSourceId}'`);
   }).catch(err => {
