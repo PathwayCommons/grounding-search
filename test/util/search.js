ecd78af1db08e833c1bca39fb55be0f2af3d05e5
jvwong <jvwong@gmail.com>
2019-04-29 10:28:21 -0400

    Adding pc blacklist for chemicals.

--- test/util/search.js
+++ test/util/search.js
@@ -2,10 +2,12 @@ const _ = require('lodash');
 
 const MOLECULAR_CELL = require('./data/molecular-cell.json');
 const TOP_NCBI_GENE = require('./data/top-ncbi-gene.json');
+const PC_BLACKLIST = require('./data/pc-blacklist.json');
 
 const SEARCH_OBJECTS = _.concat( [], 
-    // MOLECULAR_CELL, 
-    TOP_NCBI_GENE 
+    MOLECULAR_CELL, 
+    TOP_NCBI_GENE,
+    PC_BLACKLIST
   );
 
 export { SEARCH_OBJECTS };
