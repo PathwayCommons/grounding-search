aad7ba7c87acbe3388bcb71f40af8f84c637f6f6
Max Franz <maxkfranz@gmail.com>
2019-10-08 19:57:42 -0400

    Log cli task time

--- src/server/datasource/cli.js
+++ src/server/datasource/cli.js
@@ -6,12 +6,14 @@ import { uniprot } from './uniprot';
 import { chebi } from './chebi';
 import { ncbi } from './ncbi';
 import { db } from '../db';
+import { formatDistanceStrict } from 'date-fns';
 
 const sources = { uniprot, chebi, ncbi };
 const op = process.argv[2];
 const passedSourceId = process.argv[3];
 const source = sources[passedSourceId];
 const validOps = ['update', 'clear', 'index', 'download'];
+const startTime = new Date();
 
 if( !validOps.some(vo => vo === op) ){
   logger.error(`Op '${op}' not supported; try 'update' or 'clear'`);
@@ -28,7 +30,10 @@ if( op === 'clear' && passedSourceId === 'all' ){
   let fcn = source[op];
 
   fcn().then(() => {
-    logger.info(`Successfully applied ${op} on source '${passedSourceId}'`);
+    const endTime = new Date();
+    const duration = formatDistanceStrict(endTime, startTime);
+
+    logger.info(`Successfully applied ${op} on source '${passedSourceId}' in ${duration}`);
   }).catch(err => {
     logger.error(`Failed to apply ${op} on source '${passedSourceId}'`);
     logger.error(err);
