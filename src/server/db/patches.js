a2cd87998c130c542e11a2baa119e092c33cc1c2
Jeffrey Wong <jvwong@gmail.com>
2020-08-18 14:57:11 -0400

    Add patch entries for SARS-COV-2

--- src/server/db/patches.js
+++ src/server/db/patches.js
@@ -2,7 +2,272 @@ export const patches = [
   {
     namespace: 'ncbi',
     id: '43740568',
-    addSynonyms: ['spike', 'spike protein']
+    addSynonyms: [
+      'spike',
+      'spike protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1796318598',
+    addSynonyms: [
+      'spike',
+      'spike protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '43740570',
+    addSynonyms: [
+      'envelope',
+      'envelope protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1796318600',
+    addSynonyms: [
+      'envelope',
+      'envelope protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '43740571',
+    addSynonyms: [
+      'membrane',
+      'membrane protein',
+      'membrane glycoprotein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1796318601',
+    addSynonyms: [
+      'membrane',
+      'membrane protein',
+      'membrane glycoprotein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '43740575',
+    addSynonyms: [
+      'nucleocapsid',
+      'nucleocapsid protein',
+      'nucleocapsid phosphoprotein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1798174255',
+    addSynonyms: [
+      'nucleocapsid',
+      'nucleocapsid protein',
+      'nucleocapsid phosphoprotein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '43740578',
+    addSynonyms: [
+      'ORF1a polyprotein',
+      'pp1a',
+      'ORF1ab polyprotein',
+      'pp1ab'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1802476803',
+    addSynonyms: [
+      'ORF1a',
+      'ORF1a polyprotein',
+      'pp1a'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1796318597',
+    addSynonyms: [
+      'ORF1ab',
+      'ORF1ab polyprotein',
+      'pp1ab'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1826688918',
+    addSynonyms: [
+      'NSP1'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1826688919',
+    addSynonyms: [
+      'NSP2'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1826688920',
+    addSynonyms: [
+      'NSP3'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1826688921',
+    addSynonyms: [
+      'NSP4'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1826688922',
+    addSynonyms: [
+      'NSP5',
+      'Mpro'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1826688923',
+    addSynonyms: [
+      'NSP6'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1826688924',
+    addSynonyms: [
+      'NSP7'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1802476812',
+    addSynonyms: [
+      'NSP8'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1826688926',
+    addSynonyms: [
+      'NSP9'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1802476819',
+    addSynonyms: [
+      'NSP10',
+      '2\'-O-ribose methyltransferase'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1802476815',
+    addSynonyms: [
+      'NSP12'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1802476816',
+    addSynonyms: [
+      'NSP13'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1802476817',
+    addSynonyms: [
+      'NSP14',
+      '3\'-to-5\' exonuclease'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1802476818',
+    addSynonyms: [
+      'NSP15',
+      'endoRNAse'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '43740569',
+    addSynonyms: [
+      'ORF3a protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1796318599',
+    addSynonyms: [
+      'ORF3a',
+      'ORF3a protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '43740577',
+    addSynonyms: [
+      'ORF8 protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1796318604',
+    addSynonyms: [
+      'ORF8',
+      'ORF8 protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '43740572',
+    addSynonyms: [
+      'ORF6 protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1796318602',
+    addSynonyms: [
+      'ORF6',
+      'ORF6 protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '43740576',
+    addSynonyms: [
+      'ORF10 protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '1798174256',
+    addSynonyms: [
+      'ORF10',
+      'ORF10 protein'
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '',
+    addSynonyms: [
+    ]
+  },
+  {
+    namespace: 'ncbi',
+    id: '',
+    addSynonyms: [
+    ]
   }
 ];
 
