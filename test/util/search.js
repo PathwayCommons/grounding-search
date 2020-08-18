6c2011506ae3c50255b7627d2ecd86239e5fb5d9
Jeffrey Wong <jvwong@gmail.com>
2020-08-18 14:56:56 -0400

    Add test cases for SARS-COV-2

--- test/util/search.js
+++ test/util/search.js
@@ -2,11 +2,13 @@ import _ from 'lodash';
 import MOLECULAR_CELL from './data/molecular-cell.json';
 import TOP_NCBI_GENE from './data/top-ncbi-gene.json';
 import PC_BLACKLIST from './data/pc-blacklist.json';
+import SARS_COV_2 from './data/sars-cov-2.json';
 
 const SEARCH_OBJECTS = _.concat( [],
   MOLECULAR_CELL,
   TOP_NCBI_GENE,
-  PC_BLACKLIST
+  PC_BLACKLIST,
+  SARS_COV_2
 );
 
 export { SEARCH_OBJECTS };
