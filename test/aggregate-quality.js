1a507f4eab7968027b5e5706a4941f9dc6ce35e8
Jeffrey Wong <jvwong@gmail.com>
2024-07-12 21:00:51 -0400

    Include out of dictionary tests as separate class.

--- test/aggregate-quality.js
+++ test/aggregate-quality.js
@@ -51,10 +51,17 @@ describe('Search and Get Aggregate', function(){
 
               // Expected is replaced with search hit, when it is returned
               let actual = pickRecord( ground );
-              const rank = _.findIndex( results, _.matches( ground ) );
-              const found = rank >= 0;
-              if ( found ) actual = pickRecord( _.nth( results, rank ) );
 
+              // Rank is the index of the ground truth in the search results
+              // OR -1 if not present
+              // OR -2 if does not exist in the dictionary
+              let rank = _.findIndex( results, _.matches( ground ) );
+              const found = rank >= 0;
+              if ( found ){
+                actual = pickRecord( _.nth( results, rank ) );
+              } else if ( nullGround ) {
+                rank = -2;
+              }
               const message = JSON.stringify({ text, organismOrdering, actual, predicted, rank });
 
               // Compare only namespace and id
